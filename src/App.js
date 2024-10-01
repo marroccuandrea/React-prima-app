72ef0c65ab74dd98ec654a5a176a39db8048406f
marroccuandrea <marroccuandrea@yahoo.it>
2024-10-01 20:41:34 +0200

    Creazione componente basilare

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import Componente1 from "./Componente1";
 
 function App() {
   return (
     <div className="App">
       <h1>Finalmente ci siamo con React</h1>
+      <Componente1></Componente1>
     </div>
   );
 }
---
+++ src/Componente1.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Componente1 = () => {
+  return (
+    <div>
+        <h2>Ciao</h2>
+        <h2>Ciao Ciao</h2>
+    </div>
+  )
+}
+
+export default Componente1
+
+
+// Esempi di scrittura di un componente React, il contenuto si può racchiudere in un div, in un tag vuoto oppure in un React.Fragment
+
+/* const Componente1 = () => {
+  return (
+    <div> <---*
+        <h2>Ciao</h2>
+        <h2>Ciao Ciao</h2>
+    </div>
+  )
+}
+*/
+/* const Componente1 = () => {
+  return (
+    <> <---*
+        <h2>Ciao</h2>
+        <h2>Ciao Ciao</h2>
+    </>
+  )
+}
+*/
+/* const Componente1 = () => {
+  return (
+    <React.Fragment> <---*
+        <h2>Ciao</h2>
+        <h2>Ciao Ciao</h2>
+    </React.Fragment>
+  )
+}
+*/
+ 
